5dd5da99887f1158970c4191bbbc95e5cd7a55e9
EUNCHAE <2snha@naver.com>
2024-07-23 17:04:02 +0900

    [Eunchae] Edit: Start Routing

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/main/MainPage';
 import MoreMenu from './components/main/MoreMenu';
-import DiaryPage from './components/diary/DiaryPage'; // 필요한 페이지들 import
+import DiaryPage from './components/diary/DiaryPage';
+import StartPage from './components/start/StartPage';
 
 const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<StartPage />} /> {/* 기본 경로를 /로 설정 */}
                 <Route path="/main" element={<MainPage />} />
                 <Route path="/more" element={<MoreMenu />} />
                 <Route path="/diary" element={<DiaryPage />} />
@@ -15,7 +17,6 @@ const App = () => {
                 {/*<Route path="/storage" element={<StoragePage />} />*/}
                 {/*<Route path="/eye-protection" element={<EyeProtectionPage />} />*/}
                 {/*<Route path="/usage" element={<UsagePage />} />*/}
-                <Route path="/" element={<MainPage />} /> {/* 기본 경로를 /main으로 설정 */}
             </Routes>
         </Router>
     );
