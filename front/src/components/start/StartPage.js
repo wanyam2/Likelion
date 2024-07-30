98de19387b715b7fd7fd4dada852ebbd4baf7c68
dbwldh12 <uzo7383@gmail.com>
2024-07-30 17:30:26 +0900

    [유지오] 수정 내용

--- front/src/components/start/StartPage.js
+++ front/src/components/start/StartPage.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import KakaoLoginButton from './KakaoLoginButton'; // KakaoLoginButton 컴포넌트를 import
 import './StartPage.css';
+import axios from 'axios';
 
 const StartPage = () => {
+    const [text, setText] = useState('없음');
+
+    const clicked = () => {
+        axios
+            .get("http://15.164.76.9:8000/")
+            .then((response) => setText(JSON.stringify(response.data)));
+    };
+
     return (
         <div className="startpage-container">
             <div className="startpage-content">
@@ -13,17 +22,18 @@ const StartPage = () => {
                     <div>기</div>
                 </div>
                 <div className="login-options">
-                    <div className="login-option google">
+                    <div className="login-option google" onClick={clicked}>
                         <span className="icon">G</span>
                         <span className="text">구글 로그인</span>
                     </div>
                     <KakaoLoginButton /> {/* KakaoLoginButton 컴포넌트를 사용 */}
-                    <div className="login-option naver">
+                    <div className="login-option naver" onClick={clicked}>
                         <span className="icon">N</span>
                         <span className="text">네이버 로그인</span>
                     </div>
                 </div>
             </div>
+            <h1>{text}</h1>
         </div>
     );
 };
