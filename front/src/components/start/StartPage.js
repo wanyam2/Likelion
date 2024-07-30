d8bdefe80979a341d4ffdf5993357f72426b6c31
dbwldh12 <uzo7383@gmail.com>
2024-07-30 19:24:56 +0900

    [유지오] 수정 내용

--- front/src/components/start/StartPage.js
+++ front/src/components/start/StartPage.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import KakaoLoginButton from './KakaoLoginButton'; // KakaoLoginButton 컴포넌트를 import
 import './StartPage.css';
 import axios from 'axios';
 
 const StartPage = () => {
-    const [text, setText] = useState('없음');
-
-    const clicked = () => {
+    const handleClick = (loginType) => {
         axios
-            .get("http://15.164.76.9:8000/")
-            .then((response) => setText(JSON.stringify(response.data)));
+            .get('http://15.164.76.9:8000/login', {
+                params: { type: loginType }
+            })
+            .then(response => {
+                console.log(response.data);
+                // 여기서 원하는 동작을 수행할 수 있습니다.
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
     };
 
     return (
@@ -22,18 +28,17 @@ const StartPage = () => {
                     <div>기</div>
                 </div>
                 <div className="login-options">
-                    <div className="login-option google" onClick={clicked}>
+                    <div className="login-option google" onClick={() => handleClick('google')}>
                         <span className="icon">G</span>
                         <span className="text">구글 로그인</span>
                     </div>
-                    <KakaoLoginButton /> {/* KakaoLoginButton 컴포넌트를 사용 */}
-                    <div className="login-option naver" onClick={clicked}>
+                    <KakaoLoginButton onClick={() => handleClick('kakao')} /> {/* KakaoLoginButton 컴포넌트를 사용 */}
+                    <div className="login-option naver" onClick={() => handleClick('naver')}>
                         <span className="icon">N</span>
                         <span className="text">네이버 로그인</span>
                     </div>
                 </div>
             </div>
-            <h1>{text}</h1>
         </div>
     );
 };
